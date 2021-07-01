c628c8a7e602bbe5d2f8d763e1e437d431cb28ca
Yanick <tyankee15@gmail.com>
2021-07-01 16:06:25 -0300

    Present data from API on screen

--- src/components/Students.js
+++ src/components/Students.js
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 
 const Students = () => {
   const url = 'https://api.hatchways.io/assessment/students'
-  const [student, setStudent] = useState(null);
+  const [students, setStudents] = useState([]);
 
   useEffect(() => {
-    axios.get(url)
-      .then(response => {
-        setStudent(console.log(response.data))
-      })
-  }, [url])
+    fetch(url)
+      .then(res => res.json())
+      .then(
+        (results) => {
+          setStudents(results.students);
+        }
+      )
 
-
+  }, [])
 
   return (
     <div>
-      <h3>Students due assessment</h3>
-    </div>
-  );
+      {students.map((student) => (
+        <React.Fragment key={student.id}>
+          <img alt="" src={student.pic} />
+          <h1>{student.firstName} {student.lastName}</h1>
+          <p>Email:{student.email}</p>
+          <p>Company:{student.company}</p>
+          <p>Skill:{student.skill}</p>
+          <p>Average: {(student.grades.reduce((a, b) => parseInt(b) + a, 0))
+            / (student.grades.map((grade) => grade).length)}%
+                </p>
 
+        </React.Fragment>
+      ))}
+
+    </div>
+  )
 };
+
 
 export default Students;
